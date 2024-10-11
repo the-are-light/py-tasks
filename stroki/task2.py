bb6e1dd3b38918b3d575c92c638669e7eb69534b
the-are-light <artemrashu7@gmail.com>
2024-10-11 20:48:45 +0800

    Update task2.py

--- stroki/task2.py
+++ stroki/task2.py
@@ -1,5 +1,4 @@
+def lecture(s, n, m):
+    return len(set(s[i:i + m] for i in range(n-m+1)))
 
-
-n, m = map(int, input().split())
-lecture = input()
-print(len(set(lecture[i:i + m] for i in range(n-m+1))))
+print(lecture("bbaabbbabb", 10, 3))

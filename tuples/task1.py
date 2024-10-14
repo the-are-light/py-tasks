62ff6bdea5b64c6014f5f068aea4758239528a19
the-are-light <artemrashu7@gmail.com>
2024-10-14 10:49:09 +0800

    Update task1.py

--- tuples/task1.py
+++ tuples/task1.py
@@ -1,6 +1,9 @@
-s = """..*.
-.**."""
-d = s.split("\n")
+n = int(input("Введите количество строк (n): "))
+m = int(input("Введите количество символов в строке (m): "))
+
+print(f"Введите {n} строк по {m} символов:")
+d = [input() for _ in range(n)]
+
 a = [ i for id, line in enumerate(d) for i, sym in enumerate(line) if sym == "*" ]
 for id, line in enumerate(d):
     for i, j in enumerate(sorted(set(a))):

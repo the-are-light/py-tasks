930ac089aa9204875d70c9374e10c07c84f4ddb1
the-are-light <artemrashu7@gmail.com>
2024-10-14 10:54:07 +0800

    Update task2.py

--- stroki/task2.py
+++ stroki/task2.py
@@ -1,4 +1,8 @@
+
 def lecture(s, n, m):
     return len(set(s[i:i + m] for i in range(n-m+1)))
 
-print(lecture("bbaabbbabb", 10, 3))
+n, m = map(int, input("Enter values n & m: ").split())
+s = input("Enter the text of the lecture: ")
+
+print(f"Количество слов длины {m}, которые профессор мог использовать в своей лекции:", lecture(s, n, m))

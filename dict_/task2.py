1c3fa71b19a6c4b7c38de8d85079486f45648744
the-are-light <artemrashu7@gmail.com>
2024-11-20 10:27:24 +0800

    Update task2.py

--- dict_/task2.py
+++ dict_/task2.py
@@ -1,15 +1,6 @@
-text = """
-hi
-hi
-what is your name?
-my name is bond
-james bond
-my name is damme
-van damme
-claude van damme
-jean claude van damme
-"""
-
+n = int(input("Введите количество строк: "))
+text=''
+for i in range(n): text+=input()+'\n'
 dict_ = {l: text.count(l) for id, line in enumerate(text.split("\n")) for i, l in enumerate(line.split()) if l!=""}
 m = set(item for key, item in dict_.items())
 from collections import defaultdict

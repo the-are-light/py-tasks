cc08b19ae103033a966f0c58f038b3c3cc7a4604
the-are-light <artemrashu7@gmail.com>
2024-11-20 10:23:16 +0800

    Update task1.py

--- dict_/task1.py
+++ dict_/task1.py
@@ -1,17 +1,12 @@
-
-dict_ = {
-    'Ромашка': [400, 370],
-    'Василек': [600, 580],
-    'Заря': [400, 280],
-    'Малыш': [100000, 65000],
-    'Сладкоежка': [2000000, 1400000],
-    'Автолада': [500, 250]
-}
+n = int(input("Введите количество предприятий: "))
 from collections import defaultdict
-dict_2 = defaultdict(list)
-for key, item in dict_.items(): dict_2[key] = round(item[1]/item[0]*100, 2)
+dict_ = defaultdict(list)
+for i in range(n):
+    item = input("Введите название предприятий, плановый объем розничного товарооборота, фактический объем розничного товарооборота через пробел: ").split()
+    dict_[item[0]] = round(int(item[2])/int(item[1])*100, 2)
+
 count = 0
-for key, item in dict_2.items():
-    if item+10 < 100: count+=1
-for key, item in dict_2.items(): print(f"Предприятие '{key}' выполнило план на {item}%")
+for key, item in dict_.items():
+    if item+10 < 100: count +=1
+    print(f"Предприятие '{key}' выполнило план на {item}%")
 print(f"\nКоличество предприятий, недовыполнивших план на 10% и более: {count}")
